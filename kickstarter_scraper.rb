7153dbf1d3586c9864980daaba1855e18e281840
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-18 02:30:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  kickstarter.css(".post")
+  kickstarter.css("li.project.grid_4").first 
 
   
 end
