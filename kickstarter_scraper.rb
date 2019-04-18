c20cf6015a78dd73dcb8c2a8b9faa55a5e2706be
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-18 02:56:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,24 +7,28 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
+  projects = {} 
 
 
   #projects:
   kickstarter.css("li.project.grid_4").first 
   
-    kickstarter.each do |project|
-
-    #title:
-    project.css("h2.bbcard_name strong a").text
-  
+    kickstarter.each do |project| 
+      
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
+    end 
+    
+    projects 
+    
     #image link:
-    project.css("div.project-thumbnail a img").attribute("src").value
+   # project.css("div.project-thumbnail a img").attribute("src").value
   
     # description:
-    project.css("p.bbcard_blurb").text
-    binding.pry 
+   #project.css("p.bbcard_blurb").text
+    #binding.pry 
   
-  end 
+  
 end
 
 create_project_hash
